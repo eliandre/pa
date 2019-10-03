aab8283afc05f9f4dfcd76e0b2b182b40a5dcc07
Andre Eli <andre.eli@khk.ee>
2019-10-03 19:29:54 +0300

    Muutujate väljastamine

---
+++ Muutujad/valjastamine.php
@@ -0,0 +1,13 @@
+<?php
+//muutujate loomine
+$enimi = 'Karin';
+$pnimi = "Eegreid";
+$vanus = 37;
+$kaal = 69.5;
+
+// käsklus echo väljastab teksti
+echo $enimi;
+echo $pnimi;
+echo $vanus;
+echo $kaal;
+?>
