c72e1fa2367d4de07be932d95fb7e79c173148d9
Andre Eli <andre.eli@khk.ee>
2019-10-11 10:30:43 +0300

    Viskame täringut

---
+++ Tingimuslaused/taringuvise.php
@@ -0,0 +1,19 @@
+<?php
+
+$number = rand(1,6);
+
+switch($number){
+    case $number == 1: echo 'Arv on '.$number;
+    break;
+    case $number == 2: echo 'Arv on '.$number;
+    break;
+    case $number == 3: echo 'Arv on '.$number;
+    break;
+    case $number == 4: echo 'Arv on '.$number;
+    break;
+    case $number == 5: echo 'Arv on '.$number;
+    break;
+    case $number == 6: echo 'Arv on '.$number;
+    break;
+    default: echo 'Viga!';
+}
