6b401296d1e1deba311c28b948e2e5c41c1d6c23
Andre Eli <andre.eli@khk.ee>
2019-10-11 08:42:10 +0300

    Operaatorid

---
+++ Tingimuslaused/operaatorid.php
@@ -0,0 +1,30 @@
+<?php
+$x = 3;
+
+$y = 2;
+
+// loogilised operaatorid
+
+var_dump($x == $y);
+
+echo '<hr>';
+
+var_dump($x != $y);
+
+echo '<hr>';
+
+var_dump($x < $y);
+
+echo '<hr>';
+
+var_dump($x > $y);
+
+echo '<hr>';
+
+var_dump($x <= $y);
+
+echo '<hr>';
+
+var_dump($x >= $y);
+
+echo '<hr>';
