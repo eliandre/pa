67f89ee2f1c609c1214b119947d5507c48c9070d
Andre Eli <andre.eli@khk.ee>
2019-11-08 19:35:50 +0200

    Üleliigsete sulgude eemaldamine.

--- Funktsioonid/kolmnurk.php
+++ Funktsioonid/kolmnurk.php
@@ -1,8 +1,8 @@
 <?php
 function kolmnurk($a, $b){
 
-    $c = round(sqrt((pow($a,2)+pow($b,2))));
-    return "Kolmnurga kolmas külg on ".$c;
+    $c = round(sqrt(pow($a,2)+pow($b,2)));
+    return "Kolmnurga kolmas külg on ".$c.".";
 }
 
 echo kolmnurk(8,10);
