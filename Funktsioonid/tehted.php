2f35e0c180e2720e7d26ebedee1899dab6ba5f85
Andre Eli <andre.eli@khk.ee>
2019-11-08 19:14:49 +0200

    Astendamine ja trigonomeetria.

---
+++ Funktsioonid/tehted.php
@@ -0,0 +1,16 @@
+<?php
+//astendamine
+echo pow(5,2);
+echo "<br>".pow(5, 3);
+
+//ruutjuur
+echo "<br>".sqrt(9);
+
+//pii
+echo "<br>".pi();
+
+//trigonomeetria
+echo "<br>".cos(0.8);
+
+//nurgad ja radiaanid
+echo "<br>".deg2rad(30);
